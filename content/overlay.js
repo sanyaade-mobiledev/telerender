5eeaaa2b150db2ec643fc3035ccf846464ff73f0
Luc Yriarte <luc.yriarte@intel.com>
2013-01-07 22:41:29 +0100

    push content popup menu

--- content/overlay.js
+++ content/overlay.js
@@ -1,12 +1,14 @@
 var PushContent = {
-  onLoad: function() {
-    // initialization code
-    this.initialized = true;
-  },
+		
+		onLoad: function() {
+			// initialization code
+			this.initialized = true;
+		},
 
-  onMenuItemCommand: function() {
-    window.open("chrome://pushcontent/content/pushcontent.xul", "", "chrome");
-  }
+		onMenuItemCommand: function() {
+			window.open("chrome://pushcontent/content/pushcontent.xul", "", "chrome");
+		},
+
 };
 
 window.addEventListener("load", function(e) { PushContent.onLoad(e); }, false); 
