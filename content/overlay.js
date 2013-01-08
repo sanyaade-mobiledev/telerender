b9236cdf4a6316cece7efd895a54f28e467a24bd
Luc Yriarte <luc.yriarte@intel.com>
2013-01-08 17:29:42 +0100

    Remote rendering works

--- content/overlay.js
+++ content/overlay.js
@@ -1,14 +1,54 @@
 var PushContent = {
-		
-		onLoad: function() {
-			// initialization code
-			this.initialized = true;
-		},
 
-		onMenuItemCommand: function() {
-			if (document.popupNode.currentURI)
-				alert(JSON.stringify(document.popupNode.currentURI));
-		},
+	manifest: {
+		name: "pushcontent",
+		key: "demo talk",
+		permissions: ["com.intel.renderer-service-upnp"]
+	},
+
+	renderers: [],
+	
+	postInit: function() {
+		mediarenderer.setRendererListener(
+				{
+					onrendererfound: function(renderer) {
+						PushContent.renderers.push(renderer);
+					}, 
+					
+					onrendererlost: function(id) {
+						for (var i=0; i<PushContent.renderers.length; i++) {
+							if (PushContent.renderers[i].id == id) {
+								PushContent.renderers.splice(i,1);
+								return;
+							}
+						}
+					}
+				});
+		this.initialized = true;		
+	},
+	
+	onLoad: function() {
+		// initialization code
+		mediarenderer.init("ws://localhost:9000", this.manifest, this.postInit, 
+			function(err) {
+				alert("Failed to initialize server: " + err);
+		});
+	},
+
+	onMenuItemCommand: function() {
+		if (document.popupNode.currentURI && document.popupNode.currentURI.spec) {
+			for (var i=0; i<PushContent.renderers.length; i++) {
+				var renderer = PushContent.renderers[i];
+				renderer.openURI(document.popupNode.currentURI.spec,
+					function() {
+						renderer.controller.play();
+					},
+					function(err) {
+						alert(renderer.friendlyName + " failed to load " + document.popupNode.currentURI.spec + "\n" + err);
+					});
+			}
+		}
+	}
 
 };
 
--- readme.txt
+++ readme.txt
@@ -3,9 +3,24 @@ PushContent
 
 Push media content to a remote device with a popup menu.
 
+
+Usage
+-----
+
+Right click a media element on the current web page. The "Push Content" popup menu will send the URI of that element
+to all media renderers known on the network. 
+
+
 Install
 -------
 
   * Edit the pushcontent@intel.com file, replace content with its folder path on your machine.
   * Copy pushcontent@intel.com to your firefox extensions folder, for instance ~/.mozilla/firefox/cvvhptkl.default/extensions
+
+
+Running the server
+------------------
+
+	cd server
+	./cloudeebus.sh
 
