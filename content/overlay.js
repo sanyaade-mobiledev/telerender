2eb449fbe02f6504e28b3c82dc231070afeb55c4
Luc Yriarte <luc.yriarte@intel.com>
2013-01-24 16:00:04 +0100

    renderers in a dynamic cascade menu - disable if nothing to render

--- content/overlay.js
+++ content/overlay.js
@@ -27,29 +27,47 @@ var Telerender = {
 		this.initialized = true;		
 	},
 	
+	onPopupShowing: function() {
+		var contextSubMenu = document.getElementById("contentAreaContextSubMenu");
+		if (document.popupNode.currentURI && document.popupNode.currentURI.spec) {
+			contextSubMenu.disabled=false;
+			var contextSubMenuPopup = document.getElementById("contentAreaContextSubMenuPopup");
+			while (contextSubMenuPopup.firstChild)
+				contextSubMenuPopup.removeChild(contextSubMenuPopup.firstChild);
+			for (var i=0; i<Telerender.renderers.length; i++) {
+				var item = document.createElement("menuitem");
+				item.setAttribute("label",Telerender.renderers[i].friendlyName);
+				item.setAttribute("onclick","Telerender.onMenuItemCommand(" + i + ")");
+				contextSubMenuPopup.appendChild(item);
+			}
+		}
+		else {
+			contextSubMenu.disabled=true;
+		}
+	},
+	
 	onLoad: function() {
 		// initialization code
 		mediarenderer.init("ws://localhost:9000", this.manifest, this.postInit, 
 			function(err) {
 				alert("Failed to initialize server: " + err);
 		});
+		// dynamic popup menu
+		var contextMenu = document.getElementById("contentAreaContextMenu");
+		contextMenu.addEventListener("popupshowing", function() { Telerender.onPopupShowing(); }, false);
 	},
 
-	onMenuItemCommand: function() {
-		if (document.popupNode.currentURI && document.popupNode.currentURI.spec) {
-			for (var i=0; i<Telerender.renderers.length; i++) {
-				var renderer = Telerender.renderers[i];
-				renderer.openURI(document.popupNode.currentURI.spec,
-					function() {
-						renderer.controller.play();
-					},
-					function(err) {
-						alert(renderer.friendlyName + " failed to load " + document.popupNode.currentURI.spec + "\n" + err);
-					});
-			}
-		}
+	onMenuItemCommand: function(index) {
+		var renderer = Telerender.renderers[index];
+		renderer.openURI(document.popupNode.currentURI.spec,
+			function() {
+				renderer.controller.play();
+			},
+			function(err) {
+				alert(renderer.friendlyName + " failed to load " + document.popupNode.currentURI.spec + "\n" + err);
+			});
 	}
 
 };
 
-window.addEventListener("load", function(e) { Telerender.onLoad(e); }, false); 
+window.addEventListener("load", function() { Telerender.onLoad(); }, false); 
